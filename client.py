c44b39c4553a24ea50019e9b6cc57598be9a3bdc
Anilyka Barry <vgr255@live.ca>
2022-06-06 18:24:13 -0400

    Make most of the client program

---
+++ client.py
@@ -0,0 +1,32 @@
+import time
+import os
+
+path = ""
+# Uncomment the following with your Spire path
+#path = r"C:\Program Files (x86)\Steam\steamapps\common\SlayTheSpire"
+
+last = 0
+
+while True:
+    time.sleep(1)
+    possible = None
+    for file in os.listdir(os.path.join(path, "saves")):
+        if file.endswith(".autosave"):
+            if possible is None:
+                possible = file
+            else:
+                print("Error: Multiple savefiles detected.")
+                possible = None
+                break
+
+    if possible is None:
+        continue
+
+    try:
+        cur = os.path.getmtime(os.path.join(path, "saves", possible))
+    except OSError: # file got deleted by race cond
+        continue
+
+    if cur != last:
+        # XXX: send it via HTTP to the server here
+        last = cur
