9a1d50fe09a505af896407c91fbe4700c84b672e
Anilyka Barry <vgr255@live.ca>
2022-06-15 17:06:46 -0400

    Do some caching

--- client.py
+++ client.py
@@ -6,25 +6,32 @@ path = ""
 #path = r"C:\Program Files (x86)\Steam\steamapps\common\SlayTheSpire"
 
 last = 0
+possible = None
+exists = False
 
 while True:
     time.sleep(1)
-    possible = None
-    for file in os.listdir(os.path.join(path, "saves")):
-        if file.endswith(".autosave"):
-            if possible is None:
-                possible = file
-            else:
-                print("Error: Multiple savefiles detected.")
-                possible = None
-                break
+    if not exists:
+        possible = None
+        for file in os.listdir(os.path.join(path, "saves")):
+            if file.endswith(".autosave"):
+                if possible is None:
+                    possible = file
+                else:
+                    print("Error: Multiple savefiles detected.")
+                    possible = None
+                    break
+
+        if possible is not None:
+            exists = True
 
     if possible is None:
         continue
 
     try:
         cur = os.path.getmtime(os.path.join(path, "saves", possible))
-    except OSError: # file got deleted by race cond
+    except OSError:
+        exists = False
         continue
 
     if cur != last:
--- webpage.py
+++ webpage.py
@@ -1,3 +1,5 @@
+import time
+
 from aiohttp import web, ClientSession
 
 import aiohttp_jinja2
@@ -25,19 +27,20 @@ aiohttp_jinja2.setup(webpage, loader=jinja2.FileSystemLoader("Templates/"))
 
 @router.get("/")
 @aiohttp_jinja2.template("main.html")
-async def main_page(req: web.Request):
-    body = {"video_id": ""}
-    data = None
-    async with ClientSession() as session:
-        async with session.get("https://www.googleapis.com/youtube/v3/search", params=_query_params) as resp:
-            data = await resp.json()
+async def main_page(req: web.Request, _cache={"video_id": config.default_video_id, "last": 0}):
+    if _cache["video_id"] is None or _cache["last"] + config.cache_timeout < time.time():
+        data = None
+        async with ClientSession() as session:
+            async with session.get("https://www.googleapis.com/youtube/v3/search", params=_query_params) as resp:
+                data = await resp.json()
 
-    if data is not None: # TODO: fallback for failed API call?
-        body["video_id"] = data["items"][0]["id"]["videoId"]
+        if data is not None: # fallback on last/default video ID
+            _cache["video_id"] = data["items"][0]["id"]["videoId"]
+            _cache["last"] = time.time()
 
-    return body
+    return _cache
 
-import sys # TODO: Make this into a proper config module
+import sys # TODO: Make this into a proper argparse thing
 if "--webonly" not in sys.argv:
     import server
     webpage.on_startup.append(server.Twitch_startup)
