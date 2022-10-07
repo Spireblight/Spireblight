fe09e78d2151400e16f498d1bd00d4312b56ea26
Brian Yee <beeyee0401@gmail.com>
2022-10-07 15:42:28 -0500

    implement a bunch of score bonuses

--- score/RunScore.py
+++ score/RunScore.py
@@ -1,15 +1,25 @@
-from gamedata import FileParser
+import collections
+
 from score.ScoreBonus import BeyondElitesKilled, BeyondPerfect, ScoreBonus
+from save import Savefile
 
 
 class RunScore:
-    def __init__(self, run: FileParser):
+    def __init__(self, run: Savefile):
         self.run = run
-        self.bonuses: list[ScoreBonus] = []
+        # we could use this and pass to the individual bonuses, but it'd be unused for many of them
+        # prevents having to iterate over the deck a few times
+        self.card_dict = dict(collections.Counter(run.cards))
 
     def get_score_for_run(self):
-        self.bonuses.append(BeyondPerfect())
-        self.bonuses.append(BeyondElitesKilled())
+        score = 0
 
-        for bonus in self.bonuses:
-            bonus.bonus_for_run(self.run)
+        bonuses: list[ScoreBonus] = []
+        bonuses.append(BeyondPerfect())
+        bonuses.append(BeyondElitesKilled())
+        # etc
+
+        for bonus in bonuses:
+            score += bonus.bonus_for_run(self.run)
+        
+        return score
--- score/ScoreBonus.py
+++ score/ScoreBonus.py
@@ -1,6 +1,5 @@
-import collections
-
 from nameinternal import get_card_metadata, get_score_bonus
+from collections import Counter, namedtuple
 from save import Savefile
 from abc import ABC, abstractmethod
 
@@ -21,6 +20,21 @@ class ScoreBonus(ABC):
     @abstractmethod
     def bonus_for_run(self, save: Savefile):
         raise NotImplementedError
+
+    def get_starting_max_hp(self, save: Savefile):
+        max_hp_tuple = namedtuple("MaxHP", ["base_max_hp", "asc_max_hp_loss"])
+        max_hp_dict = {
+            "Ironclad": max_hp_tuple(80, 5),
+            "Silent": max_hp_tuple(70, 4),
+            "Defect": max_hp_tuple(75, 4),
+            "Watcher": max_hp_tuple(72, 4)
+        }
+
+        character_max_hp = max_hp_dict[save.character].base_max_hp
+        if save.ascension_level >= 14:
+            character_max_hp -= max_hp_dict[save.character].asc_max_hp_loss
+
+        return character_max_hp
 
 class Ascension(ScoreBonus):
     def __init__(self):
@@ -85,14 +99,12 @@ class Champion(ScoreBonus):
         """Return 25 points for each elite perfected.""" 
         return 25 * save._data.get("champions", 0)
 
-
-
 class BossesSlain(ScoreBonus):
     def __init__(self):
         super().__init__("Bosses Slain")
 
     def bonus_for_run(self, save: Savefile):
-        # do this based on floor number?
+        # do this based on floor number or path == "BOSS"?
         return 0
 
 class BeyondPerfect(ScoreBonus):
@@ -111,7 +123,7 @@ class Collector(ScoreBonus):
     
     def bonus_for_run(self, save: Savefile):
         """Have 4 copies of any non-starter card.""" 
-        card_dict = dict(collections.Counter(save.cards))
+        card_dict = dict(Counter(save.cards))
         nonstarter_cards = [key for key, value in card_dict.items() if value >= 4 and get_card_metadata(key)["RARITY"] != "Starter"]
         return 25 * len(nonstarter_cards)
 
@@ -140,14 +152,169 @@ class Overkill(ScoreBonus):
         super().__init__("Overkill")
     
     def bonus_for_run(self, save: Savefile):
+        """Deal 99 damage with a single attack.""" 
         if save._data.get("overkill", False):
             return 25
         return 0
 
-# I don't think this one will be used in an ongoing run?
+class MysteryMachine(ScoreBonus):
+    def __init__(self):
+        super().__init__("Mystery Machine")
+    
+    def bonus_for_run(self, save: Savefile):
+        """Visited 15+ events.""" 
+        if save._data.get("mystery_machine", 0) >= 15:
+            return 25
+        return 0
+
+class ILikeShiny(ScoreBonus):
+    def __init__(self):
+        super().__init__("Shiny")
+    
+    def bonus_for_run(self, save: Savefile):
+        """Has 25+ relics.""" 
+        if len(save._data.get("relics", [])) >= 25:
+            return 50
+        return 0
+
+class WellFed(ScoreBonus):
+    def __init__(self):
+        super().__init__("Well Fed")
+    
+    def bonus_for_run(self, save: Savefile):
+        """Has 15+ additional Max HP."""
+        if save._data.get("max_health", 0) - self.get_starting_max_hp(save) > 15:
+            return 25
+        return 0
+
+class Stuffed(ScoreBonus):
+    def __init__(self):
+        super().__init__("Stuffed")
+    
+    def bonus_for_run(self, save: Savefile):
+        """Has 30+ additional Max HP."""
+        if save._data.get("max_health", 0) - self.get_starting_max_hp(save) > 30:
+            return 50
+        return 0
+
+class MoneyMoney(ScoreBonus):
+    def __init__(self):
+        super().__init__("Money Money")
+    
+    def bonus_for_run(self, save: Savefile):
+        """Gained 1000+ gold."""
+        if save._data.get("gold_gained", 0) >= 1000:
+            return 25
+        return 0
+
+class RainingMoney(ScoreBonus):
+    def __init__(self):
+        super().__init__("Raining Money")
+    
+    def bonus_for_run(self, save: Savefile):
+        """Gained 2000+ gold."""
+        if save._data.get("gold_gained", 0) >= 2000:
+            return 50
+        return 0
+
+class ILikeGold(ScoreBonus):
+    def __init__(self):
+        super().__init__("I Like Gold")
+    
+    def bonus_for_run(self, save: Savefile):
+        """Gained 3000+ gold."""
+        if save._data.get("gold_gained", 0) >= 3000:
+            return 75
+        return 0
+
+class Combo(ScoreBonus):
+    def __init__(self):
+        super().__init__("Combo")
+    
+    def bonus_for_run(self, save: Savefile):
+        """Play 20 cards in a single turn."""
+        if save._data.get("combo", False):
+            return 25
+        return 0
+
+class Pauper(ScoreBonus):
+    def __init__(self):
+        super().__init__("Pauper")
+    
+    def bonus_for_run(self, save: Savefile):
+        """Have 0 rare cards."""
+        rare_cards = [card for card in save.cards if get_card_metadata(card)["RARITY"] == "Rare"]
+        if not rare_cards:
+            return 50
+        return 0
+
+class Curses(ScoreBonus):
+    def __init__(self):
+        super().__init__("Curses")
+    
+    def bonus_for_run(self, save: Savefile):
+        """Your deck contains 5 curses."""
+        curses = [card for card in save.cards if get_card_metadata(card)["CHARACTER"] == "Curse"]
+        if len(curses) >= 5:
+            return 100
+        return 0
+
+class Highlander(ScoreBonus):
+    def __init__(self):
+        super().__init__("Highlander")
+    
+    def bonus_for_run(self, save: Savefile):
+        """Your deck contains no duplicates."""
+        card_dict = dict(Counter(save.cards))
+        multiple_copies = [key for key, value in card_dict.items() if value > 1 and get_card_metadata(key)["RARITY"] != "Starter"]
+        if not multiple_copies:
+            return 100
+        return 0
+
+class Poopy(ScoreBonus):
+    def __init__(self):
+        super().__init__("Poopy")
+    
+    def bonus_for_run(self, save: Savefile):
+        """Possess Spirit Poop."""
+        relics = save._data.get("relics", [])
+        if "Spirit Poop" in relics:
+            return -1
+        return 0
+
+# Probably never include any of the below in an ongoing run?
+class Speedster(ScoreBonus):
+    def __init__(self):
+        super().__init__("Speedster")
+    
+    def bonus_for_run(self, save: Savefile):
+        """Victory in under 60 minutes."""
+        if save._data.get("metric_playtime", 0) < 3600:
+            return 25
+        return 0
+
+class LightSpeed(ScoreBonus):
+    def __init__(self):
+        super().__init__("Light Speed")
+    
+    def bonus_for_run(self, save: Savefile):
+        """Victory in under 60 minutes."""
+        if save._data.get("metric_playtime", 0) < 2700:
+            return 50
+        return 0
+
 class Heartbreaker(ScoreBonus):
     def __init__(self):
         super().__init__("Heartbreaker")
     
     def bonus_for_run(self, save: Savefile):
         return 250
+
+class OnMyOwnTerms(ScoreBonus):
+    def __init__(self):
+        super().__init__("On My Own Terms")
+    
+    def bonus_for_run(self, save: Savefile):
+        """Killed yourself."""
+        # no idea how this is checked or if we care
+        return 50
