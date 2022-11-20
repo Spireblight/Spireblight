439667cd4fee1f958b673ae0669c1c96d3e0b0ab
Olivia Thiderman <olivia@thiderman.org>
2022-11-20 19:22:24 +0100

    Change refresh method to use HTMX, every 10s
    This will make it not reload the full page anymore, only the content.

--- response_objects/run_single.py
+++ response_objects/run_single.py
@@ -8,8 +8,9 @@ if TYPE_CHECKING:
     from runs import RunParser
 
 class RunResponse:
-    def __init__(self, parser: FileParser, run_linked_node: RunLinkedListNode = None, *, autorefresh: bool, redirect: bool):
+    def __init__(self, parser: FileParser, run_linked_node: RunLinkedListNode = None, *, autorefresh: bool, redirect: bool, extend_base: bool):
         self.run = parser
         self.linked_runs = run_linked_node
         self.autorefresh = autorefresh
         self.redirect = redirect
+        self.extend_base = extend_base
