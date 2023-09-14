4fc2beddda28f14d99d7b6df1ae6b7cc5b17bf46
Virtual256 <virtual256@gmail.com>
2023-09-13 18:49:06 -0700

    Add os specific config.spire.steamdir test

---
+++ test/test_config.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+
+from platform import system
+from os.path import expanduser
+
+from configuration import config
+
+class TestConfigFile(TestCase):
+
+    def test_default_spire_steamdir(self):
+        system_os = system().lower()
+        if system_os == "windows":
+            self.assertEqual(config.spire.steamdir, 'C:\Program Files (x86)\Steam\steamapps\common\SlayTheSpire')
+        elif system_os == "linux":
+            self.assertEqual(config.spire.steamdir, expanduser("~/.steam/steam/steamapps/common/SlayTheSpire"))
+        else:
+            self.fail(f"No default spire.steamdir set for os: '{system_os}'\nSet spire steamdir manually in config file.")
