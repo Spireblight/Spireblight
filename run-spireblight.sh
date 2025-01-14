84524e99430d8ad77b59e379b13e0d4d1388422e
Ben Wirth <benjaminwirth10@gmail.com>
2025-01-13 23:51:48 -0800

    Docker compose for running the Python application -
    Implement a bearbones Docker setup.
    
    Simple configuration for the networking (`host`) and fs. The working directory for the Python app is mapped to the root of the repo, so in-theory there should be very minimal side-effects.
    
    Also left some slots for future additions to the compose - the idea with just implementing the compose is that its likely that other services like Postgres are dependencies to the project which should just be ready to go instead of needing to install sep.

---
+++ run-spireblight.sh
@@ -0,0 +1,5 @@
+# /bin/bash
+
+# requires this to be ran at the root of the repository - including this script as it is probable that other steps may be taken (for configuration, checks, CD, etc.)
+
+docker compose up --build -d
