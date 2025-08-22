726666cbfca790109c9d8cc953cc5d8fed4e579a
Anilyka Barry <vgr255@live.ca>
2025-08-22 16:23:38 -0400

    Fix a bunch of stuff

--- src/archive.py
+++ src/archive.py
@@ -15,7 +15,7 @@ from src.events import add_listener
 from src.utils import getfile
 from src.runs import get_parser, RunParser, _cache
 
-_date_re = re.compile(r"\(\d\d\d\d\-\d\d\-\d\d\)")
+_date_re = re.compile(r"(\d\d\d\d\-\d\d\-\d\d)")
 _chapter_re = re.compile(r"^(\d\d?):(\d\d):(\d\d) Slay the Spire \- (\w+)$")
 
 archive: _ArchiveHandler = None
@@ -34,6 +34,9 @@ class Run:
         self.run = run
         self.start = start
         self.vod = vod
+
+    def __repr__(self):
+        return f"ArchiveRun<{self.run}>"
 
     def to_json(self):
         return {"id": self.run.name, "start": self.start}
@@ -55,7 +58,10 @@ class VOD:
         return hash(self.id)
 
     def __eq__(self, value):
-        return isinstance(value, VOD) and value.id == self.id
+        return (
+            (isinstance(value, VOD) and value.id == self.id) or
+            (isinstance(value, str) and value == self.id)
+        )
 
     def to_json(self):
         return {**self.data.__dict__, "runs": self.runs}
@@ -68,7 +74,7 @@ class VOD:
                 h, m, s, char = c.groups()
                 minutes = int(m)
                 minutes += int(h) * 60
-                seconds = int(s) + minutes
+                seconds = int(s) + minutes * 60
                 final.append( (seconds, char) )
 
         return final
@@ -177,59 +183,79 @@ class _ArchiveHandler:
             for run, (offset, char) in zip(runs, offsets):
                 if run.character == char:
                     final_runs.append(Run(run, offset, vod))
+                    run.vod = vod
 
             vod.runs.extend(final_runs)
 
     async def load_from_api(self) -> bool:
         """Fetch VOD information from YouTube. Return True if it succeeded."""
         search_params = {
-            "part": "id", 
-            "order": "date",
+            "part": "snippet",
             "maxResults": 50,
-            "channelId": self._channel_id,
+            "playlistID": "UU" + self._channel_id[2:], # easy
             "key": self._key,
+        }
+        video_params = {
+            "part": "contentDetails",
+            "maxResults": 50,
+            "key": self._key,
+            "id": [],
         }
 
         if self._session is None:
             self._session = ClientSession("https://youtube.googleapis.com/youtube/v3/")
 
+
+        all_videos: dict[str, tuple[str, str]] = {}
         while True:
             search_resp = None
-            async with self._session.get("search", params=search_params) as resp:
-                search_resp: dict = await resp.json()
+            async with self._session.get("playlistItems", params=search_params) as resp:
+                if resp.ok:
+                    search_resp: dict = await resp.json()
 
             if not search_resp:
                 return False
 
-            # get metadata and save it in a VideoMetadata file
             for item in search_resp["items"]:
-                info = item.get("id")
-                if info is None or not (c := info.get("videoId")):
-                    continue
+                snippet = item.get("snippet")
+                if snippet is None:
+                    continue # ??
 
+                all_videos[snippet["resourceId"]["videoId"]] = (snippet["title"], snippet["description"])
+
+            next_token = search_resp.get("nextPageToken")
+            if not next_token:
+                break
+            search_params["pageToken"] = next_token
+
+        video_params["id"].extend(all_videos.keys() - self.vods)
+
+        while True:
+            video_resp = None
+            async with self._session.get("videos", params=video_params) as resp:
+                if resp.ok:
+                    video_resp: dict = await resp.json()
+
+            if not video_resp:
+                return False
+
+            for item in video_resp["items"]:
+                c = item["id"]
+                title, description = all_videos[c]
                 vod = VOD(c)
                 if vod in self.vods:
                     return True # we already have this one, so anything after this is older and we have it
 
-                video_resp = None
-                async with self._session.get("videos", params={"part": "snippet,contentDetails", "id": c, "key": self._key}) as resp:
-                    video_resp = await resp.json()
-
-                if not video_resp:
-                    return False
-
-                video_info = video_resp["items"][0]
-                snippet = video_info["snippet"]
-                if not _date_re.search(snippet["title"]): # doesn't have a date in the title, not a vod
+                if not _date_re.search(title): # doesn't have a date in the title, not a vod
                     continue
-                duration: str = video_info["contentDetails"]["duration"][2:-1] # string in the format PT#H#M#S; remove PT and S
+                duration: str = item["contentDetails"]["duration"][2:-1] # string in the format PT#H#M#S; remove PT and S
                 hours, H, rest = duration.partition("H")
                 if not H: # if it's <1 hour
                     hours, rest = "", hours
                 minutes, M, seconds = rest.partition("M")
                 total_minutes = int(hours or 0) * 60 + int(minutes or 0)
                 total_seconds = total_minutes * 60 + int(seconds or 0)
-                vod.data = VideoMetadata(c, snippet["title"], total_seconds, snippet["description"])
+                vod.data = VideoMetadata(c, title, total_seconds, description)
 
                 self.vods.add(vod)
                 date = vod.datetime.date()
@@ -237,10 +263,10 @@ class _ArchiveHandler:
                     self.unparsed[date] = []
                 self.unparsed[date].append(vod)
 
-            next_token = search_resp.get("nextPageToken")
+            next_token = video_resp.get("nextPageToken")
             if not next_token:
                 break
-            search_params["pageToken"] = next_token
+            video_resp["pageToken"] = next_token
 
         return True
 
