93f34ced7b09c0a0e42e1ae976182c49d814b123
Anilyka Barry <vgr255@live.ca>
2025-09-29 14:35:07 -0400

    No CLI shorthands

--- src/config.py
+++ src/config.py
@@ -28,14 +28,11 @@ config: _cfgmap.Config
 
 parser = argparse.ArgumentParser(__botname__)
 
-parser.add_argument("--config-file", "-f", action="append", help="Add a config file to use")
-parser.add_argument("--version", "-v", action="version", version=f"{__botname__} {__version__}")
+parser.add_argument("--config-file", action="append", help="Add a config file to use")
+parser.add_argument("--version", action="version", version=f"{__botname__} {__version__}")
 parser.add_argument("--no-twitch", action="store_true", help="Disable the Twitch bot")
 parser.add_argument("--no-discord", action="store_true", help="Disable the Discord bot")
-parser.add_argument("--channel", "-c", help="Which Twitch channel to join")
-
-# safeguard against Sphinx, so we don't error during docgen
-parser.add_argument("-M", nargs=3, help="For internal use only, do not use")
+parser.add_argument("--channel", help="Which Twitch channel to join")
 
 def load_default_config():
     global curpath
@@ -96,8 +93,9 @@ def load_user_config(conf: _cfgmap.Config, args: argparse.Namespace):
 
 def load():
     global config
-    args = parser.parse_args()
-    assert args.M is None or len(args.M) == 3 # None if normal, 3 if Sphinx. ignore it
+    # this is important; both testing and docgen use their own args
+    # we should only care about what we actually use
+    args, _ = parser.parse_known_args()
 
     config = load_default_config()
     load_user_config(config, args)
