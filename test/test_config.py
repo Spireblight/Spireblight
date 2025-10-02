3db387d5c9c8e22e5586db27fb7f1fa791cff759
Anilyka Barry <vgr255@live.ca>
2025-10-01 21:49:09 -0400

    More tests

--- test/test_config.py
+++ test/test_config.py
@@ -1,15 +1,19 @@
 from unittest import TestCase
 from pathlib import Path
 
-from src import config as _cfgmodule
+import logging
+
+from src import logger, config as _cfgmodule
 from src.exceptions import InvalidConfigType
+
+# this is where all the test files reside
+orig = Path.cwd() / "test" / "static"
 
 class TestConfigFile(TestCase):
     def setUp(self):
         self.config = _cfgmodule.load_default_config()
 
     def test_auto_disable(self):
-        orig = Path.cwd() / "test" / "static"
         file_enabled = orig / "with-tokens.yml"
         file_disabled = orig / "auto-disable.yml"
 
@@ -25,6 +29,57 @@ class TestConfigFile(TestCase):
 
         self.assertFalse(self.config.twitch.enabled)
         self.assertFalse(self.config.discord.enabled)
+
+    def test_exists(self):
+        # works fine
+        self.config.update({"bot": {"prefix": "%"}})
+
+        with self.assertLogs(logger.logger, logging.WARNING):
+            self.config.update({"spam": "eggs"})
+
+        with self.assertRaises(AttributeError):
+            self.config.spam
+
+    def test_replace(self):
+        self.assertEqual(self.config.bot.prefix, "!")
+        self.config.update({"bot": {"prefix": "%"}})
+        self.assertEqual(self.config.bot.prefix, "%")
+
+    def test_nested(self):
+        self.assertEqual(self.config.twitch.extended.client_id, "")
+        self.config.update({"twitch": {"extended": {"client_id": "eggs"}}})
+        self.assertEqual(self.config.twitch.extended.client_id, "eggs")
+
+    def test_dict_type(self):
+        # this should just succeed
+        self.config.update({"twitch": {"channel": "spam"}})
+
+        with self.assertRaises(InvalidConfigType):
+            self.config.update({"twitch": "eggs"})
+
+        with self.assertRaises(InvalidConfigType):
+            self.config.update({"server": "spamalot"})
+
+    def test_list_type(self):
+        lst = self.config.bot.spire_mods
+        self.assertEqual(lst, ["Slay the Spire"])
+        self.config.update({"bot": {"spire_mods": "Downfall"}})
+        self.assertEqual(self.config.bot.spire_mods, ["Slay the Spire", "Downfall"])
+
+        self.assertIs(self.config.bot.spire_mods, lst)
+
+        self.config.update({"bot": {"spire_mods": {"Packmaster"}}})
+        self.assertEqual(self.config.bot.spire_mods, ["Slay the Spire", "Downfall", "Packmaster"])
+
+        self.assertIs(self.config.bot.spire_mods, lst)
+
+        self.config.update({"bot": {"spire_mods": ["Downfall", "Hermit"]}})
+        self.assertEqual(self.config.bot.spire_mods, ["Slay the Spire", "Downfall", "Packmaster", "Hermit"])
+
+        self.assertIs(self.config.bot.spire_mods, lst)
+
+        with self.assertRaises(InvalidConfigType):
+            self.config.update({"bot": {"spire_mods": 34}})
 
     def tearDown(self):
         self.config = None
