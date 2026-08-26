76e78fa79cca6321222c1602b5148106d0d5fd1e
Anilyka Barry <vgr255@live.ca>
2026-08-26 14:47:30 -0400

    Finalize most of the core Spotify code

--- src/spotify.py
+++ src/spotify.py
@@ -1,9 +1,12 @@
 """Module to handle talking to the Spotify API.
 
-This exposes a single class, which should not be instantiated more than once."""
+This exposes a single instance, and the main class should not be instantiated more than once."""
 
 from aiohttp import ClientSession, ContentTypeError
+from aiohttp.web import Request, Response, HTTPForbidden
+
 import datetime
+import secrets
 import base64
 import json
 
@@ -12,7 +15,22 @@ from src.config import config
 from src.logger import logger
 from src.utils import getfile
 
-__all__ = ["Spotify"]
+from src.webpage import router
+
+__all__ = ["spotify"]
+
+@router.get("/spotify/oauth2")
+async def get_new_tokens(req: Request):
+    params = req.query
+    if spotify.token_handler.state != params["state"]:
+        raise HTTPForbidden(reason="Invalid request state")
+
+    spotify.token_handler.state = None # no matter what, we're done with it
+
+    if "error" in params:
+        raise HTTPForbidden(reason=params["error"])
+
+    await spotify.token_handler.get_new_access_token(code=params["code"])
 
 class TokenHandler:
     _filename_default: str = "spotify_tokens.json" #: JSON file under the data/ folder where tokens will be saved.
@@ -22,14 +40,17 @@ class TokenHandler:
         self._refresh_token: str = None
         self._session: ClientSession = None
         self._expires_at: datetime.datetime = None
+        self.state = None
 
     async def get_token(self):
         """Get the Spotify token used for accessing the API.
 
         If the token is expired, obtain a new one. Otherwise, return the one saved on disk."""
+
         if self._token is None:
             self.load_tokens()
         if self._expires_at < datetime.datetime.now(): # expired, get another one
+            self._token = None
             await self.get_new_access_token()
         return self._token
 
@@ -80,12 +101,25 @@ class TokenHandler:
             if fd is not None:
                 fd.close()
 
-    async def get_new_access_token(self):
-        """Obtain a new access token using the refresh token."""
-        params = {
-            "grant_type": "refresh_token",
-            "refresh_token": self._refresh_token,
-        }
+    async def get_new_access_token(self, *, code=None):
+        """Obtain a new access token using the refresh token.
+
+        This doesn't return any value."""
+
+        if self._refresh_token is None and code is None: # need first auth
+            return
+
+        if code is None:
+            params = {
+                "grant_type": "refresh_token",
+                "refresh_token": self._refresh_token,
+            }
+        else:
+            params = {
+                "grant_type": "authorization_code",
+                "code": code,
+                "redirect_uri": f"{config.server.url}/spotify/oauth2",
+            }
 
         value = base64.urlsafe_b64encode(f"{config.spotify.id}:{config.spotify.secret}".encode("utf-8")).decode("utf-8")
 
@@ -98,13 +132,24 @@ class TokenHandler:
             data = await resp.json()
             if resp.ok:
                 self.populate_tokens(data)
-            else:
-                await self.request_authentication()
 
-    async def request_authentication(self):
+    def request_authentication_link(self): # TODO: make client use this
         """Request authentication from Spotify API and pass it on to the client.
 
         This is used once at first, and then whenever the refresh token expires."""
+
+        self.state = secrets.token_urlsafe(16)
+
+        params = {
+            "client_id": config.spotify.id,
+            "response_type": "code",
+            "redirect_uri": f"{config.server.url}/spotify/oauth2",
+            "state": self.state,
+            "scope": config.spotify.scopes,
+            "show_dialog": "false", # whether to prompt every time
+        }
+
+        return ("https://accounts.spotify.com/authorize", params)
 
     def populate_tokens(self, data: dict[str, str | int]):
         """Tokens freshly obtained from Spotify."""
@@ -112,6 +157,8 @@ class TokenHandler:
         self._token = data["access_token"]
         self._refresh_token = data["refresh_token"]
         # scopes are space-delimited, but storing them as a list is more convenient
+        # we technically have the list of scopes in config, but they might be wrong
+        # they should be equal at all times, but on the off chance they're not, we save them here
         self._scopes = data["scope"].split()
         now = datetime.datetime.now()
         expires = datetime.timedelta(seconds=data["expires_in"])
@@ -127,18 +174,16 @@ class Spotify:
         """The aiohttp session to connect to the Spotify API."""
         return self.token_handler.session
 
-    async def authenticate(self, *, force=False):
-        """Trigger a handshake to get new OAuth2 credentials.
+    async def authenticate(self):
+        """Trigger a handshake to get new OAuth2 credentials."""
 
-        :param force: Whether to force a re-authentication.
-        :type force: bool
-        """
+        return self.token_handler.request_authentication_link()
 
     async def now_playing(self):
         """Obtain the currently-playing song using the Spotify API."""
         token = await self.token_handler.get_token()
         if token is None: # for whatever reason
-            return None
+            return {}
 
         async with self.session.get(
             "https://api.spotify.com/v1/me/player/currently-playing",
@@ -154,66 +199,4 @@ class Spotify:
                 return {}
 
 
-
-
-
-
-
-
-
-
-    ### old code
-
-    async def refresh_spotify_token(self):
-        if not config.spotify.enabled:
-            return
-
-        if self._session is None:
-            self._session = ClientSession()
-
-        value = base64.urlsafe_b64encode(
-            f"{config.spotify.id}:{config.spotify.secret}".encode("utf-8")
-        )
-        value = value.decode("utf-8")
-
-        headers = {
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Authorization": f"Basic {value}",
-        }
-
-        if self._spotify_refresh_token:
-            params = {
-                "grant_type": "refresh_token",
-                "refresh_token": self._spotify_refresh_token,
-            }
-
-        else:
-            params = {
-                "grant_type": "authorization_code",
-                "code": config.spotify.code,
-                "redirect_uri": f"{config.server.url}/spotify",
-            }
-
-        async with self._session.post(
-            "https://accounts.spotify.com/api/token", headers=headers, params=params
-        ) as resp:
-            if resp.ok:
-                content = await resp.json()
-                self._spotify_token = content["access_token"]
-                self._expires_at = (
-                    datetime.datetime.now()
-                    + datetime.timedelta(seconds=content["expires_in"])
-                ).timestamp()
-                if "refresh_token" in content:
-                    self._spotify_refresh_token = content["refresh_token"]
-                    try:
-                        with open(
-                            os.path.join("data", "spotify_refresh_token"), "w"
-                        ) as f:
-                            f.write(self._spotify_refresh_token)
-                    except OSError:  # oh no
-                        logger.error(
-                            f"Could not write refresh token to file: {self._spotify_refresh_token}"
-                        )
-                return self._spotify_token
-            return None
+spotify = Spotify()
